caab1c364a767a11a0290d6d60f88d14e5ac4461
Brian Sheridan <bsheridan12@gmail.com>
2015-02-13 19:34:45 -0600

    add working associations and basic controll flow

--- app/controllers/dashboard.rb
+++ app/controllers/dashboard.rb
@@ -1,4 +1,6 @@
 
 get '/dashboard' do
+  @user = User.find_by(id: session[:user_id])
+
   erb :'/users/index'
 end
--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -1,23 +1,24 @@
 get '/surveys' do
-  erb :'/surveys/index.erb'
+  erb :'/surveys/index'
 end
 
 get '/surveys/new' do
-  erb :'/surveys/new.erb'
+  erb :'/surveys/new'
 end
 
 post '/surveys/new' do
+  @user = User.find_by(id: session[:user_id])
   new_survey = Survey.new params[:survey]
+  new_survey.creator_id = @user.id
+  # all_questions = params[:questions]
 
-  all_questions = params[:questions]
-
-  all_questions.each do |question|
-    new_question = Question.new question: question.ask, survey_id: new_survey.id
-    question.choices.each do |choice|
-      Response.new choice: choice, question_id: new_question.id
-    end
-  end
-
+  # all_questions.each do |question|
+  #   new_question = Question.new question: question.ask, survey_id: new_survey.id
+  #   question.choices.each do |choice|
+  #     Response.new choice: choice, question_id: new_question.id
+  #   end
+  # end
+  new_survey.save
   erb :'/surveys/show'
 end
 
